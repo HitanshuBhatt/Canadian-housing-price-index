23c979dee376091d966ec969cb734b3bca4cb481
HitanshuBhatt <hitanshusbhatt@gmail.com>
2026-04-15 15:43:48 -0600

    re created Django project

--- housing_project/housing_project/asgi.py
+++ housing_project/housing_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for canadian_housing_price_dashboard project.
+ASGI config for housing_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'canadian_housing_price_dashboard.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'housing_project.settings')
 
 application = get_asgi_application()
--- housing_project/housing_project/urls.py
+++ housing_project/housing_project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for canadian_housing_price_dashboard project.
+URL configuration for housing_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
