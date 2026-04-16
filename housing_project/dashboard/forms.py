ae34c3629cca95f21404a2f7790c686b9b70d957
HitanshuBhatt <hitanshusbhatt@gmail.com>
2026-04-16 17:47:19 -0600

    fixed comments

--- housing_project/dashboard/forms.py
+++ housing_project/dashboard/forms.py
@@ -1,16 +1,21 @@
 from django import forms
 
-
+#  form used to upload csv file
 class CSVUploadForm(forms.Form):
+    #  input field to select file 
     csv_file = forms.FileField(label="Upload a CSV file")
 
+# validatio for uploaded file
     def clean_csv_file(self):
+        #  get uploaded file from cleaned from from data
         csv_file = self.cleaned_data["csv_file"]
 
+# check if it is a .csv file 
         if not csv_file.name.lower().endswith(".csv"):
             raise forms.ValidationError("Please upload a valid CSV file.")
 
+#  check if file contains data and is not empty by checking size 
         if csv_file.size == 0:
             raise forms.ValidationError("The uploaded file is empty.")
-
+#  return the validated file 
         return csv_file
