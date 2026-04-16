c006bce2790e551ec1657257a38e7473c1c66ada
HitanshuBhatt <hitanshusbhatt@gmail.com>
2026-04-16 15:53:44 -0600

    added charts

--- housing_project/dashboard/models.py
+++ housing_project/dashboard/models.py
@@ -1,20 +1,11 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
 
 class HousingData(models.Model):
-    # DATE column (e.g., 2020-01-01)
-    date = models.DateField()
-    
-    # GEO column (Geography/Location)
-    geo = models.CharField(max_length=255)
-    
-    # CATEGORY column (e.g., house)
-    category = models.CharField(max_length=100)
-    
-    # VALUE column (Housing price index)
-    value = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.CharField(max_length=20)
+    geo = models.CharField(max_length=100)
+    category = models.CharField(max_length=50)
+    value = models.FloatField()
 
     def __str__(self):
-        return f"{self.geo} - {self.date} ({self.value})"
+        return f"{self.geo} - {self.date}"
