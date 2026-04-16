5ee80c2bd26cc3c0f09bb224ea9c9450a300c277
HitanshuBhatt <hitanshusbhatt@gmail.com>
2026-04-16 16:17:34 -0600

    fixed datafield and value field to allow decimals

--- housing_project/dashboard/models.py
+++ housing_project/dashboard/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class HousingData(models.Model):
-    date = models.CharField(max_length=20)
+    date = models.DateField()
     geo = models.CharField(max_length=100)
     category = models.CharField(max_length=50)
-    value = models.FloatField()
+    value = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return f"{self.geo} - {self.date}"
