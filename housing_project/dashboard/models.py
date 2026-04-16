b66a2de000b7ccc47214b1689ca500584ac9eb89
HitanshuBhatt <hitanshusbhatt@gmail.com>
2026-04-16 17:56:05 -0600

    added comments

--- housing_project/dashboard/models.py
+++ housing_project/dashboard/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 
-
+# model creted to store the housing data based on the csv uploaded
 class HousingData(models.Model):
+    # date of housing record 
+
     date = models.DateField()
-    geo = models.CharField(max_length=100)
-    category = models.CharField(max_length=50)
+    #  name of location
+    geo = models.CharField(max_length=255)
+    # name of category e.g. house 
+    category = models.CharField(max_length=100)
+    #  value of index 
     value = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
