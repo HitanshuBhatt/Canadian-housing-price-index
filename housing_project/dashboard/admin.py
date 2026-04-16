1c19724cd798c3a715e3effb857026c1d6413d31
HitanshuBhatt <hitanshusbhatt@gmail.com>
2026-04-16 17:42:10 -0600

    added housingData admin Settings  and added comments

--- housing_project/dashboard/admin.py
+++ housing_project/dashboard/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import HousingData
 
-# Register your models here.
+
+@admin.register(HousingData)
+class HousingDataAdmin(admin.ModelAdmin):
+    list_display = ("date", "geo", "category", "value")
+    list_filter = ("geo", "category", "date")
+    search_fields = ("geo", "category")
+    ordering = ("-date", "geo")
