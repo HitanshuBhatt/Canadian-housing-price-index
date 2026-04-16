43e98df4ee557c49bed627909fa972c77de80b72
HitanshuBhatt <hitanshusbhatt@gmail.com>
2026-04-16 17:42:40 -0600

    added comments

--- housing_project/dashboard/admin.py
+++ housing_project/dashboard/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from .models import HousingData
 
-
+#  register housing data model
 @admin.register(HousingData)
 class HousingDataAdmin(admin.ModelAdmin):
+    #  show columns in admin table view 
     list_display = ("date", "geo", "category", "value")
+    #  add filter on the right side in admin panel 
     list_filter = ("geo", "category", "date")
+    #  add serach fieled
     search_fields = ("geo", "category")
+    #  show latest data first with date first 
     ordering = ("-date", "geo")
